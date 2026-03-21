631762627f0cad6ef3d70c8829ca6bdaa4dba622
MaxzyMVT <148692733+MaxzyMVT@users.noreply.github.com>
2026-03-22 01:41:41 +0700

    build: config NextConfig for environment variable

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    FRONTEND_URL: process.env.FRONTEND_URL,
+    BACKEND_URL: process.env.BACKEND_URL,
+  }
 };
 
 export default nextConfig;

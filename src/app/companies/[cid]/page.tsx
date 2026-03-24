72675047d508d4bf4ce4e356743933fbc42be272
MaxzyMVT <148692733+MaxzyMVT@users.noreply.github.com>
2026-03-24 16:05:55 +0700

    fix: cid params is now a promise

--- src/app/companies/[cid]/page.tsx
+++ src/app/companies/[cid]/page.tsx
@@ -4,20 +4,22 @@ import getCompany from "@/libs/getCompany";
 import AdminCompanyDetail from "@/components/AdminCompanyDetail";
 import UserCompanyDetail from "@/components/UserCompanyDetail";
 
-export default async function CompanyDetailPage({ params }: { params: { cid: string } }) {
-  const session = await getServerSession(authOptions);
-  const company = await getCompany(params.cid);
+export default async function CompanyDetailPage({ params }: { params: Promise<{ cid: string }> }) {
+  
+    const { cid } = await params;
+    const session = await getServerSession(authOptions);
+    const company = await getCompany(cid);
 
-  const role = session?.user?.role;
-  const token = session?.user?.token;
+    const role = session?.user?.role;
+    const token = session?.user?.token;
 
-  return (
-    <main className="relative min-h-screen flex flex-col items-center pt-24">
-      {
-        role === "admin"
-          ? <AdminCompanyDetail company={company} adminToken={token} />
-          : <UserCompanyDetail company={company} token={token} />
-      }
-    </main>
-  );
+    return (
+        <main className="relative min-h-screen flex flex-col items-center pt-24">
+        {
+            role === "admin"
+            ? <AdminCompanyDetail company={company} adminToken={token} />
+            : <UserCompanyDetail company={company} token={token} />
+        }
+        </main>
+    );
 }

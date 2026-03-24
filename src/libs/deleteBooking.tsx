2abd3981175e9e9e55093afd33b594507960f1b0
MaxzyMVT <148692733+MaxzyMVT@users.noreply.github.com>
2026-03-24 16:34:47 +0700

    fix: wrong return type

--- src/libs/deleteBooking.tsx
+++ src/libs/deleteBooking.tsx
@@ -1,6 +1,4 @@
-import { BookingItem } from "../../interfaces";
-
-export default async function deleteBooking(id: string, token: string): Promise<BookingItem>{
+export default async function deleteBooking(id: string, token: string) {
     const response = await fetch(`${process.env.BACKEND_URL}/api/v1/bookings/${id}`,
         {
             method: "DELETE",
